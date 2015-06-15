aa0795c8ef5b9ef34c10661e9584234ca68f79ca
Ramiro Oliva <rosvain@gmail.com>
2015-06-15 13:42:34 -0400

    defining problems on script.js file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//DETECT FONT RE-SIZE
+//vision impaired 
+//size of type to small for user with low visibility
+
+//DETECT SCREEN RE-SIZE
+//vision repaired
+//low visibility
+
+//DETECT FOR HIGH CONTRAST MODE
+//vision impaired
+//low visibility
+
+//DETECT USAGE OF SKIP TO CONTENT LINK
+//vision impair
+//identify if there are skip to content link
+//identify if user click on skip to content link
+//how many times does user click on the skip to content link
+
+
+//DETECT USAGE OF CAPTIONING (HTML 5 VIDEO)
+//hearing impaired
+//identify if captions are use on page
+//identify if user interacs with caption
+
+
+//DETECT KEYBOARD VS MOUSE EVENTS
+//vision impaired
+
