3bab520c459f04bbe3c3096ebb19c4ab2f0c8de9
MIkal Morello <mikalmorello@gmail.com>
2015-06-15 14:09:26 -0400

    Added javascript code outline

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,43 @@
 //DETECT FONT RE-SIZE
 //vision impaired 
-//size of type to small for user with low visibility
+//Identify if user increases default font text sizes in order to increase legibility
+
+
 
 //DETECT SCREEN RE-SIZE
 //vision repaired
-//low visibility
+//identify if user modifies the screen size to increase legibility
+ 
+
 
 //DETECT FOR HIGH CONTRAST MODE
 //vision impaired
-//low visibility
+//identify if user increases the screen contrast, or modifies site presentation to increase legibility
+
+
 
 //DETECT USAGE OF SKIP TO CONTENT LINK
 //vision impair
 //identify if there are skip to content link
-//identify if user click on skip to content link
+//identify if user clicks on skip to content link
 //how many times does user click on the skip to content link
+// *this could be expanded to include all hidden screen reader only content
+
 
 
 //DETECT USAGE OF CAPTIONING (HTML 5 VIDEO)
 //hearing impaired
-//identify if captions are use on page
-//identify if user interacs with caption
+//identify if captions are utiized on the page
+//identify if user interacts with caption
+
 
 
 //DETECT KEYBOARD VS MOUSE EVENTS
 //vision impaired
+//identify if the primary means of navigating the page is through the keyboard or mouse
+//identify the types of keyboard interactions and track onfocus events to indiciate if accessibility tools are being used
 
+
+
+//STORE ANALYTICS DATA
+//Collect and store data so that it can be represented in an administrative interface for the user to analyze
