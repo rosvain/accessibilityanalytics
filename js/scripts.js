58566deb17cc977a0e74c65d4fa1e1ef48cc6a2a
Mikal Morello <mikalmorello@gmail.com>
2015-06-29 22:34:55 -0400

    Added skip to content tracking and beginning of on focus tracking

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,14 @@ $( window ).resize(function() {
 //identify if user clicks on skip to content link
 //how many times does user click on the skip to content link
 // *this could be expanded to include all hidden screen reader only content
+    var skipCount = 0;
+	$(document).ready(function(){
+		$(".skip").click(function(){
+			console.log("Skip to content link clicked");
+			skipCount++
+			console.log("Skip to content Count:"+" "+skipCount);
+		});
+	});
 
 
 
@@ -43,31 +51,41 @@ $( window ).resize(function() {
 //DETECT KEYBOARD VS MOUSE EVENTS
 //vision impaired
 //identify if the primary means of navigating the page is through the keyboard or mouse
+
 //identify the types of keyboard interactions and track onfocus events to indiciate if accessibility tools are being used
-// Log all keyboard interactions
+	//var focus = $("..").is(":focus");
+	//console.log(focus);
+
+// Log all keyboard interactions (will have to detect input fields first)
+    var hasInput = ($(document).has("input").length ? "Yes" : "No" );
 	var keyCount = 0;
 	$(document).keypress(function(event) {
 	  console.log(event.key);
 	  keyCount++
 	  console.log("Key Count:"+" "+keyCount);
+	  return keyCount;
 	});
+	
 // Log all click interactions
     var clickCount = 0;
 	$(document).click(function(event) {
 	  console.log(event.type);
 	  clickCount++
 	  console.log("Click Count:"+" "+clickCount);
+	  return clickCount;
 	});
+	
 // Compare key count vs click count
-	var compareInteractions = function(){
+		var compareInteractions = $(document).ready(function(){
 		if(keyCount > clickCount){
 			console.log("User interacting primarily through keyboard interactions ");
 		} else if (keyCount < clickCount){
 			console.log("User interacting primarily through mouse interactions ");
 		} else {
-			console.log("Undetermined or equal");
+			console.log("User interactions undetermined or equal");
 		}
-	}
+	});
+	
 
 //STORE ANALYTICS DATA
 //collect and store data so that it can be represented in an administrative interface for the user to analyze
