29d341572a4b8da6ed14b4167da7693cadcd40d9
Ramiro Oliva <rosvain@gmail.com>
2015-07-22 13:37:36 -0400

    testing session behavior

--- app/session.php
+++ app/session.php
@@ -7,6 +7,15 @@ function start_session() {
 }
 
 start_session();
+
+$old_sessionid = session_id();
+
+session_regenerate_id();
+
+$new_sessionid = session_id();
+
+echo "Old Session: $old_sessionid<br />";
+echo "New Session: $new_sessionid<br />";
 $_SESSION["username"] = "Callum";
     session_regenerate_id();
 echo '<pre>';
