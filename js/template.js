f37229e1c2fe234d56c887af29f6bb61c6c50fa5
Mikal Morello <mikalmorello@gmail.com>
2015-07-28 23:50:00 -0400

    Updated sidebar navigation, removed some extra html content, added javascript to support the menu and transition states, added icon font

--- js/template.js
+++ js/template.js
@@ -3,19 +3,28 @@
 
 // FADE IN BODY ON PAGE LOAD
 var opacity = function() {
-	  $('body').css('opacity', '0').fadeTo(1100, 1,'swing'); 
+	  $('body').css('opacity', '0').fadeTo(800, 1,'swing'); 
 };
 	
 // SLIDE IN LEFT NAVIGATION
 var main = function() {
   $('.icon-menu').click( function(){
-      $('nav').animate({left: '0px'}, 200);
-      $('body').animate({left: '285px'}, 200);
+	  if($('nav').hasClass("active")){
+		 $('nav').animate({left: '-285px'}, 100).removeClass("active");
+         $('body').animate({left: '0px'}, 100);
+		 $('article').fadeTo(200, 1);
+      }else{
+        $('nav').animate({left: '0px'}, 100).addClass("active");
+		$('body').animate({left: '285px'}, 100);
+		$('article').fadeTo(200, .5);
+      }
   });
   $('.icon-close').click(function(){
-    $('nav').animate({left: '-285px'}, 200);
-    $('body').animate({left: '0px'}, 200);
+    $('nav').animate({left: '-285px'}, 100).removeClass("active");
+    $('body').animate({left: '0px'}, 100);
+	$('article').fadeTo(200, 1);
   });
 };
+
 $(document).ready(main);
 $(document).ready(opacity);
