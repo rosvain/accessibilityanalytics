280f559bfc9f42ccd9f4686b0faf2e7888d18822
Ramiro Oliva <rosvain@gmail.com>
2015-08-19 11:08:54 -0400

    getting ip and session id

--- app/session.php
+++ app/session.php
@@ -1,25 +1,65 @@
 <?php
 
-function start_session() {
-    session_start();
-    //$SESSION['name'] ='john';
-    
-}
+session_start();
 
-start_session();
+//$SESSION['name'] ='john';
 
-$old_sessionid = session_id();
+$sessionid = session_id();
 
-session_regenerate_id();
+//session_regenerate_id();
+//$new_sessionid = session_id();
 
-$new_sessionid = session_id();
-
-echo "Old Session: $old_sessionid<br />";
-echo "New Session: $new_sessionid<br />";
-$_SESSION["username"] = "Callum";
-    session_regenerate_id();
+echo "Old Session: $sessionid<br />";
+//echo "New Session: $new_sessionid<br />";
+$_SESSION["username"] = "pilgrim";
+//session_regenerate_id();
 echo '<pre>';
 var_dump($_SESSION);
+var_dump($_SERVER);
 echo '</pre>';
+
+function get_clientserver_ip() {
+    $ipaddress = '';
+    if ($_SERVER['HTTP_CLIENT_IP'])
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if ($_SERVER['HTTP_X_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if ($_SERVER['HTTP_X_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if ($_SERVER['HTTP_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if ($_SERVER['HTTP_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if ($_SERVER['REMOTE_ADDR'])
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+function get_clientenv_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+$ip =  get_clientserver_ip();
+echo $ip;
+
+$ip =  get_clientenv_ip();
+echo $ip;
 
 session_destory();
