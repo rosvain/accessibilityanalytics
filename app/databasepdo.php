4be67e1bc2f628f055664f363defcfab95c238ac
Ramiro Oliva <rosvain@gmail.com>
2015-08-24 21:39:52 -0400

    removing slim/views, not necessary right now, maybe later. Querying and loopoing thought all results from user table

--- app/databasepdo.php
+++ app/databasepdo.php
@@ -10,3 +10,15 @@ try {
     echo $ex->getMessage();
     die();
 }
+try {
+    $results = $db->query('select * from user');
+} catch (Exception $ex) {
+    echo $ex->getMessage();
+}
+
+
+$users = $results->fetchAll(PDO::FETCH_ASSOC);
+foreach ($users as $user){
+    echo "{$user['ip']} <br>";
+}
+
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Slim\\Views\\' => array($vendorDir . '/slim/views'),
 );
